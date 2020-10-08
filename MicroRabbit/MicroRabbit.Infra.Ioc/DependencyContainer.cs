5200810ecd0108e95c534a05af7dd0a770ac8908
Navin Samuel <Navin Samuel@DESKTOP-U5585RS>
2020-10-08 17:57:44 +0530

    Infrastrcuture project - initisl commit

---
+++ MicroRabbit/MicroRabbit.Infra.Ioc/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroService.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Infra.Ioc
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain bus
+
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
